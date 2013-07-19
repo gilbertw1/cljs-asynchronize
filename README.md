e15707fc1492b2499decb7ba1b721f1b0935f8cb
Bryan Gilbert <gilbertw1@gmail.com>
2013-07-19 13:42:14 -0400

    documentation updates

--- README.md
+++ README.md
@@ -1,13 +1,3 @@
-# github-node-cljs
+# cljs-asynchronize
 
-A Clojure library designed to ... well, that part is up to you.
-
-## Usage
-
-FIXME
-
-## License
-
-Copyright © 2013 FIXME
-
-Distributed under the Eclipse Public License, the same as Clojure.
+A cljs library designed to asynchronize yo code
--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject cljs-defasync "0.1.0-SNAPSHOT"
   :source-paths ["comp/clojurescript/src/clj"
      						 "comp/clojurescript/src/cljs"]
-	:description "FIXME: write description"
-	:url "http://example.com/FIXME"
+	:description "Asynchronize callback code"
+	:url ""
 	:license {:name "Eclipse Public License"
 			:url "http://www.eclipse.org/legal/epl-v10.html"}
 	:plugins [[lein-cljsbuild "0.3.2"]
